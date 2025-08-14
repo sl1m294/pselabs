9be8da38e13e8e9961862eea6f3ed12058f06967
psal-afk <p_salimo@fanshaweonline.ca>
2025-08-14 01:11:29 -0400

    added about page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,7 +1,60 @@
 import React from "react";
+import AboutImg from "../img/about/about.jpg";
+import { Link } from "react-router-dom";
 
 const About = () => {
-  return <section className="section  min-h-screen">About</section>;
+  return (
+    <section className="section">
+      <div className="container mx-auto h-full relative">
+        {/* text & Image */}
+        <div className=" flex flex-col lg:flex-row h-full items-center justify-center gap-x-24 text-center lg:text-left lg:pt-16">
+          {/* image */}
+          <div className="flex-1 max-h-96 lg:max-h-max order-2 lg:order-none overflow-hidden">
+            <img src={AboutImg} alt="woman2" />
+          </div>
+          {/* text */}
+          <div className="flex-1 pt-36 pb-14 lg:pt-0 lg:w-auto z-10 flex flex-col justify-center items-center lg:items-start ">
+            <h1 className="h1">About Me</h1>
+            <p>
+              I’m a passionate photographer and filmmaker based in London,
+              Ontario, dedicated to capturing authentic moments and telling
+              compelling stories through visuals. With a keen eye for detail and
+              a love for creativity, I specialize in portrait, lifestyle, and
+              cinematic photography, blending artistry with technical precision.
+            </p>
+            <br />
+            <br />
+            <p>Let's work together and create epic memories</p>
+            <Link to={"/portfolio"} className="btn">
+              view my work{" "}
+            </Link>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default About;
+
+// <div className="container mx-auto h-full relative">
+//   {/* Text and Image Wrapper */}
+//   <div className="flex flex-col justify-center">
+//     {/* Text */}
+//     <div className="w-full pt-36 pb-14 lg:pt-0 lg:pb-0 lg:pl-10 lg:w-auto z-10 lg:absolute flex flex-col justify-center items-center lg:items-start">
+//       <h1 className="h1">
+//         photographer <br /> & film maker
+//       </h1>
+//       <p className="text-[26px] ">London, ON</p>
+//       <Link to={"/contact"} className="btn mb-[30px]">
+//         lets talk
+//       </Link>
+//     </div>
+//     {/* Image */}
+//     <div className="flex justify-end max-h-96 lg:max-h-max">
+//       <div className="relative lg:-right-40 overflow-hidden">
+//         <img src={HeroImg} alt="hero_image" />
+//       </div>
+//     </div>
+//   </div>
+// </div>;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 //image
-import HeroImg from "../img/home/woman6.png";
+import HeroImg from "../img/home/woman3.png";
 
 import { Link } from "react-router-dom";
 
@@ -8,22 +8,29 @@ const Home = () => {
   return (
     <section className="section">
       <div className="container mx-auto h-full relative">
-        {/* Text and Image Wrapper */}
+        {/* text and image wrapper */}
         <div className="flex flex-col justify-center">
-          {/* Text */}
-          <div className="w-full pt-36 pb-14 lg:pt-0 lg:pb-0 lg:pl-10 lg:w-auto z-10 lg:absolute flex flex-col justify-center items-center lg:items-start">
-            <h1 className="h1">
+          {/* text */}
+          <div className="w-full pt-36 pb-14 lg:pt-0 lg:pb-0 lg:w-auto z-10 lg:absolute flex flex-col justify-center items-center lg:items-start">
+            <h1 className="text-[54px] lg:text-[108px] font-primary font-bold capitalize leading-[120%] tracking-[-0.05em] mb-2">
               photographer <br /> & film maker
             </h1>
-            <p className="text-[26px] ">London, ON</p>
+
+            <p className="text-[26px] lg:text-[36px] font-primary mb-4 lg:mb-12">
+              London, ON
+            </p>
             <Link to={"/contact"} className="btn mb-[30px]">
-              lets talk
+              lets connect
             </Link>
           </div>
-          {/* Image */}
-          <div className="flex justify-end max-h-96 lg:max-h-max">
-            <div className="relative lg:-right-40 overflow-hidden">
-              <img src={HeroImg} alt="hero_image" />
+          {/* image */}
+          <div className="flex justify-end">
+            <div>
+              <img
+                src={HeroImg}
+                alt="hero image"
+                className="w-[400px] lg:w-[800px]"
+              />
             </div>
           </div>
         </div>
