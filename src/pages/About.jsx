8bff772d8960473f8072e1d301d6111f0a9957c1
psal-afk <p_salimo@fanshaweonline.ca>
2025-08-14 01:18:13 -0400

    image added

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import AboutImg from "../img/about/about.jpg";
+import AboutImg from "../img/about/about.JPG";
 import { Link } from "react-router-dom";
 
 const About = () => {
